5465489a75327af77e137042a0c52a2bcc9262f9
Averouse <vero04122005@gmail.com>
2025-12-26 19:15:17 +0700

    Second Fix mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
-export default defineConfig({
-  site: '[https://samiratravel-pangkalanbanteng.vercel.app](https://samiratravel-pangkalanbanteng.vercel.app)', // Ganti dengan URL asli nanti
-  integrations: [tailwind()],
-  vite: {
-    plugins: []
-  }
-});
+export default defineConfig({});

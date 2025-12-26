08b7cb2f8749f5beb7f908d24b4c7844638c5ec4
Averouse <vero04122005@gmail.com>
2025-12-26 19:17:39 +0700

    Final fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-// astro.config.mjs
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
